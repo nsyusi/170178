b41d1e4ff47a100c66903b1f72753626ee0e0d63
Yusei2017 <33210407+nsyusi@users.noreply.github.com>
2017-11-27 10:07:37 +0900

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+	public class Hello2{
+		public static void main(String[] args){
+			JFrame f = new JFrame("window");
+			f.setBounds(500,500,500,500);
+			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			f.setVisible(true);
+		}
+	}
