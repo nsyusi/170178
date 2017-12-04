cd4a1f07dd7d983c71e916d030efc6db3287be08
Yusei2017 <33210407+nsyusi@users.noreply.github.com>
2017-12-04 11:31:30 +0900

    LargerFrame.java added.

--- LargeFrame.java
+++ LargeFrame.java
@@ -5,6 +5,7 @@ public class LargeJFrame extends JFrame{
 		setLocation(0,0);
 		setSize(1000,1000);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	public static void main(String[] args){
 		LargeFrame f = new LargeFrame("ALOHA");
 		f.setVisible(true);
